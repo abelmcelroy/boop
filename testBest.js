451063795ba92b8c6396d49df260538aeba29fab
year-of-the <abel.h.mcelroy0@gmail.com>
2025-09-19 00:03:24 -0400

    adding a way to test a saved net

---
+++ testBest.js
@@ -0,0 +1,31 @@
+const NegamaxAlphaBeta = require("negamax-alpha-beta");
+const { Network } = require("neataptic");
+const { Game } = require("./boop.js");
+const fs = require("fs");
+
+const SEARCH_DEPTH = 3;
+
+let negamaxConfig = {
+  generateMoves: (game) => game.current.availableMoves(),
+  makeMove: (game, move) => game.makeMove(...move) || true,
+  unmakeMove: (game) => game.unmakeMove(),
+  evaluate: (game) => game.current.playerTurn ? -game.current.board.score() : game.current.board.score(),
+  evaluateTerminal: (game) => game.current.winner === game.current.playerTurn ? Infinity : game.current.winner !== null ? -Infinity : null,
+};
+
+const negamax = new NegamaxAlphaBeta(negamaxConfig);
+
+function play() {
+  const saved = JSON.parse(fs.readFileSync("best_net.json", "utf8"));
+  const net1 = Network.fromJSON(saved);
+  const net2 = Network.fromJSON(saved);
+  const game = new Game(net1, net2);
+  while (!game.isOver) {
+    let { bestMove } = negamax.search(game, SEARCH_DEPTH);
+    game.makeMove(...bestMove);
+  }
+  return game;
+}
+
+const finishedGame = play();
+finishedGame.peekHistory();
